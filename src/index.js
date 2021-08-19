7f760c80647fcb6ba0612d3f6a251c3af3ffde64
Yuliia Kharchenko <jkharchenko2008@gmail.com>
2021-08-19 11:18:04 +0300

    тренировка настроек стилей

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import './sass/main.scss';
 import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
-import { defaults } from '@pnotify/core';
 
 defaultModules.set(PNotifyMobile, {});
 
 alert({
   text: 'ошибка бла бла бла!',
 });
-
-defaults.styling = 'material';
