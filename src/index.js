f75e67a1bc4801b49f32a5381258c040aa40933a
Yuliia Kharchenko <jkharchenko2008@gmail.com>
2021-08-19 10:29:07 +0300

    установка pnotify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './sass/main.scss';
-// import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
-// // import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import { defaults } from '@pnotify/core';
 
-// alert({
-//   text: 'Notice me, senpai!',
-// });
+defaultModules.set(PNotifyMobile, {});
+
+alert({
+  text: 'ошибка бла бла бла!',
+});
+
+defaults.styling = 'material';
