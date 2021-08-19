8f19b57d3d43e926180d89e3ab09b3dcad3348c7
Yuliia Kharchenko <jkharchenko2008@gmail.com>
2021-08-19 10:12:26 +0300

    базовые настройки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+// import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+// // import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+
+// alert({
+//   text: 'Notice me, senpai!',
+// });
