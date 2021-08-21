3b3de2bdb90ceec338a941ee92a261865a3b2720
Yuliia Kharchenko <jkharchenko2008@gmail.com>
2021-08-21 21:13:34 +0300

    чистит код

--- src/index.js
+++ src/index.js
@@ -4,30 +4,17 @@ import getRefs from './refs';
 
 import { error, defaultModules, Stack } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
-// import { func } from 'assert-plus';
 
 import { debounce } from 'debounce';
 
 defaultModules.set(PNotifyMobile, {});
 
-// const defaultStack = new Stack({
-//   dir1: 'down',
-//   dir2: 'left',
-//   hide: true,
-//   delay: 100,
-//   mouseReset: true,
-//   remove: true,
-// });
-
 const refs = getRefs();
-console.log(refs.container);
-console.log(refs.input);
 
 const handlerInput = e => {
   refs.container.innerHTML = '';
 
   const search = e.target.value.trim();
-  console.log('search', search);
 
   fetchCountry(search)
     .then(countries => renderCollection(countries))
@@ -35,11 +22,6 @@ const handlerInput = e => {
 };
 
 refs.input.addEventListener('input', debounce(handlerInput, 500));
-
-// fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
-//   .then(response => response.json())
-//   .then(coctails => renderCollection(coctails.drinks))
-//   .catch(err => console.log(err));
 
 function renderCollection(arr) {
   if (arr.length === 1) {
@@ -54,11 +36,9 @@ function renderCollection(arr) {
     });
   }
   if (arr.length === undefined) {
-    console.log(arr.length);
     const text = `<p class='not-found'>Please enter a valid request</p>`;
     refs.container.insertAdjacentHTML('beforeend', text);
   }
-  console.log(arr.length);
 }
 
 function createList(arr) {
@@ -80,8 +60,7 @@ function createItem({ name, capital, population, languages, flag }) {
         <ul class="language-list">Languages:
           ${getLanguages(languages)}
         </ul>
-      </div>
-    
+      </div>   
       <img class="country__flag" src="${flag}" alt="${name}"/>
     </div>
   </div>`;
@@ -92,7 +71,3 @@ function createItem({ name, capital, population, languages, flag }) {
 function getLanguages(languages) {
   return languages.map(({ name }) => `<li class="language-list__item">${name}</li>`).join(' ');
 }
-
-// function createItem() {
-//   return cardsTemplate();
-// }
